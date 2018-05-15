a11a6baf2895e850b1ab81dc8b53291f7dea16f6
HiskiHuovila <38249172+HiskiHuovila@users.noreply.github.com>
2018-05-15 15:59:44 +0300

    Update phat.py
    changed "temperature" to "message"

--- iot/phat.py
+++ iot/phat.py
@@ -8,7 +8,7 @@ import time
 
 def readSensors():
     sensors = {
-        "temperature": envirophat.weather.temperature()
+        "message": envirophat.weather.temperature()
     }
 
     return sensors 
